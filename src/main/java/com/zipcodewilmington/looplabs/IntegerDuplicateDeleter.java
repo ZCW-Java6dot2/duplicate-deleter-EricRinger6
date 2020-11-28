d7d18de74d746699dcbb5952ccd7bd05be3738a8
EricRinger6 <eric.ringer6@gmail.com>
2020-11-28 16:36:13 -0500

    Finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,72 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int size = 0;
+        Integer occur = 0;
+
+        if(maxNumberOfDuplications == 1 || maxNumberOfDuplications == 0){
+            return new Integer[]{};
+        }
+
+        for(Integer i : array){
+            occur = getNumOfOccurances(i);
+            if(occur < maxNumberOfDuplications){
+                size++;
+            }
+        }
+
+        if(size == 0){
+            return new Integer[]{};
+        }
+
+        Integer[] newArr = new Integer[size];
+        int index = 0;
+
+        for(Integer i : array){
+            if(getNumOfOccurances(i) < maxNumberOfDuplications){
+                newArr[index] = i;
+                index++;
+            }
+        }
+        return newArr;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int size = 0;
+
+        for(Integer i : array){
+            if(!getNumOfOccurances(i).equals(exactNumberOfDuplications)){
+                size++;
+            }
+        }
+
+        int index = 0;
+        Integer[] newArr = new Integer[size];
+
+        for(Integer i : array) {
+            if(!getNumOfOccurances(i).equals(exactNumberOfDuplications)) {
+                newArr[index] = i;
+                index++;
+            }
+        }
+
+        return newArr;
+    }
+
+    public Integer getNumOfOccurances(Integer num){
+        Integer counter = 0;
+        for(Integer i : array){
+            if(i.equals(num)){
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,73 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int size = 0;
+        Integer occur = 0;
+
+        if(maxNumberOfDuplications == 1 || maxNumberOfDuplications == 0){
+            return new String[]{};
+        }
+
+        for(String s : array){
+            occur = getNumOfOccurances(s);
+            if(occur < maxNumberOfDuplications){
+                size++;
+            }
+        }
+
+        if(size == 0){
+            return new String[]{};
+        }
+
+        String[] newArr = new String[size];
+        int index = 0;
+
+        for(String s : array){
+            occur = getNumOfOccurances(s);
+            if(occur < maxNumberOfDuplications){
+                newArr[index] = s;
+                index++;
+            }
+        }
+        return newArr;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int size = 0;
+
+        for(String s : array){
+            if(!getNumOfOccurances(s).equals(exactNumberOfDuplications)){
+                size++;
+            }
+        }
+
+        int index = 0;
+        String[] newArr = new String[size];
+
+        for(String s : array) {
+            if(!getNumOfOccurances(s).equals(exactNumberOfDuplications)) {
+                newArr[index] = s;
+                index++;
+            }
+        }
+
+        return newArr;
+    }
+
+    public Integer getNumOfOccurances(String string){
+        Integer counter = 0;
+        for(String s : array){
+            if(s.equals(string)){
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
